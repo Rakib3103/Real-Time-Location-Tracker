75d5b958edaf607d3f3b4ed4aa03ef363eb96a99
Mazharul Islam Rakib <63310221+Rakib3103@users.noreply.github.com>
2022-10-06 21:11:20 +0600

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,341 @@
+const labels = [
+    '0',
+    '2',
+
+  ];
+
+ 
+
+//1
+
+
+  const data1 = {
+    labels: labels,
+    datasets: [{
+      label: 'Oxygen Concentration',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0.6, 0.8, 1, 1.2, 1.4],
+    }]
+  };
+
+  const config1 = {
+    type: 'line',
+    data: data1,
+    options: {}
+  };
+
+  const myChart1 = new Chart(
+    document.getElementById('myChart1'),
+    config1
+  );
+
+// 2
+
+const data2 = {
+    labels: labels,
+    datasets: [{
+      label: 'Hydrogen Concentration',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config2 = {
+    type: 'line',
+    data: data2,
+    options: {}
+  };
+
+  const myChart2 = new Chart(
+    document.getElementById('myChart2'),
+    config2
+  );
+
+// 3
+
+const data3 = {
+    labels: labels,
+    datasets: [{
+      label: 'light',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config3 = {
+    type: 'line',
+    data: data3,
+    options: {}
+  };
+
+  const myChart3 = new Chart(
+    document.getElementById('myChart3'),
+    config3
+  );
+//4
+const data4 = {
+    labels: labels,
+    datasets: [{
+      label: 'UV ray',
+      backgroundColor: 'rgb(0, 0, 0)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config4 = {
+    type: 'line',
+    data: data1,
+    options: {}
+  };
+
+  const myChart4 = new Chart(
+    document.getElementById('myChart4'),
+    config1
+  );
+
+//5
+const data5 = {
+    labels: labels,
+    datasets: [{
+      label: 'Atmospheric Pressure',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config5 = {
+    type: 'line',
+    data: data5,
+    options: {}
+  };
+
+  const myChart5 = new Chart(
+    document.getElementById('myChart5'),
+    config5
+  );
+
+//6
+const data6 = {
+    labels: labels,
+    datasets: [{
+      label: 'Carbon Monoxide',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config6 = {
+    type: 'line',
+    data: data6,
+    options: {}
+  };
+
+  const myChart6 = new Chart(
+    document.getElementById('myChart6'),
+    config6
+  );
+
+//7
+const data7 = {
+    labels: labels,
+    datasets: [{
+      label: 'Methane Concentration',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config7 = {
+    type: 'line',
+    data: data7,
+    options: {}
+  };
+
+  const myChart7 = new Chart(
+    document.getElementById('myChart7'),
+    config7
+  );
+
+//8
+const data8 = {
+    labels: labels,
+    datasets: [{
+      label: 'Flammable gas',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config8 = {
+    type: 'line',
+    data: data8,
+    options: {}
+  };
+
+  const myChart8 = new Chart(
+    document.getElementById('myChart8'),
+    config8
+  );
+
+//9
+const data9= {
+    labels: labels,
+    datasets: [{
+      label: 'Temperature',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config9 = {
+    type: 'line',
+    data: data9,
+    options: {}
+  };
+
+  const myChart9 = new Chart(
+    document.getElementById('myChart9'),
+    config9
+  );
+
+//10
+const data10= {
+    labels: labels,
+    datasets: [{
+      label: 'Moisture',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config10 = {
+    type: 'line',
+    data: data10,
+    options: {}
+  };
+
+  const myChart10 = new Chart(
+    document.getElementById('myChart10'),
+    config10
+  );
+
+
+//11
+const data11= {
+    labels: labels,
+    datasets: [{
+      label: 'Air Quality',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config11 = {
+    type: 'line',
+    data: data11,
+    options: {}
+  };
+
+  const myChart11 = new Chart(
+    document.getElementById('myChart11'),
+    config11
+  );
+
+//12
+const data12= {
+    labels: labels,
+    datasets: [{
+      label: 'Carbon Dioxide',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config12 = {
+    type: 'line',
+    data: data12,
+    options: {}
+  };
+
+  const myChart12 = new Chart(
+    document.getElementById('myChart12'),
+    config12
+  );
+
+//13
+const data13= {
+    labels: labels,
+    datasets: [{
+      label: 'Smoke',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config13 = {
+    type: 'line',
+    data: data13,
+    options: {}
+  };
+
+  const myChart13 = new Chart(
+    document.getElementById('myChart13'),
+    config13
+  );
+
+//14
+const data14= {
+    labels: labels,
+    datasets: [{
+      label: 'Alcohol',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config14 = {
+    type: 'line',
+    data: data14,
+    options: {}
+  };
+
+  const myChart14 = new Chart(
+    document.getElementById('myChart14'),
+    config14
+  );
+
+//15
+const data15= {
+    labels: labels,
+    datasets: [{
+      label: 'Ammonia',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 50, 2, 20, 30],
+    }]
+  };
+
+  const config15 = {
+    type: 'line',
+    data: data15,
+    options: {}
+  };
+
+  const myChart15 = new Chart(
+    document.getElementById('myChart15'),
+    config15
+  );
